bfe5d22d9d3ca560cfa25612cc5cc0fee47f094d
HungTruong4689 <hungphuc4689@gmail.com>
2025-01-03 09:58:24 +0200

    question 002

---
+++ exercise/part1/question002.js
@@ -0,0 +1,6 @@
+//What is a prototype chain
+
+// create new type of objects based on existing ones
+// inheritance prototype chain (properties and methods)
+// prototype chain is a series of objects connected by the prototype property
+// available through the prototype property of the constructor function Object.getPrototypeOf(object) or object.__proto__
