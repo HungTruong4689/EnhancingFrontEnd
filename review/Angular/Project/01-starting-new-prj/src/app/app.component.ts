21bea9f321c8bb72f358623c9ddb703549d1995f
HungTruong4689 <hungphuc4689@gmail.com>
2025-02-02 12:05:27 +0700

    binding data

--- review/Angular/Project/01-starting-new-prj/src/app/app.component.ts
+++ review/Angular/Project/01-starting-new-prj/src/app/app.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UsersComponent } from "./users/users.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UsersComponent],
+  imports: [HeaderComponent, UsersComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectUser(){
+    return this.users.find( user => user.id === this.selectedUserId )!;
+  }
+
+
+  onSelectUser( id:string ){
+    this.selectedUserId  =  id;
+  }
 }
--- review/Angular/Project/01-starting-new-prj/src/app/users/users.component.ts
+++ review/Angular/Project/01-starting-new-prj/src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component,signal,computed,Input } from '@angular/core';
+import { Component,signal,computed,Input,input,Output, EventEmitter } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const random_index = Math.floor(Math.random()*DUMMY_USERS.length);
@@ -11,15 +11,17 @@ const random_index = Math.floor(Math.random()*DUMMY_USERS.length);
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+  @Input({required:true}) id!:string ;
   @Input({required:true}) avatar!:string ;
   @Input({required:true}) name!:string ;
+  @Output() select = new EventEmitter();
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
   }
 
   onSelectUser(  ){
-     
+     this.select.emit(this.id); // pass id to parent componentng
     // this.selectedUser = DUMMY_USERS[Math.floor(Math.random()*DUMMY_USERS.length)];
   }
 }
